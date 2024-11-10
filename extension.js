1b3fc063456e73de1988362156042412fb1b8099
ChangHu <66352520+changhu73@users.noreply.github.com>
2024-11-10 15:54:00 +0800

    generate translation

--- extension.js
+++ extension.js
@@ -1,39 +1,101 @@
-// // The module 'vscode' contains the VS Code extensibility API
-// // Import the module and reference it with the alias vscode in your code below
+
 // const vscode = require('vscode');
+// const { execFile } = require('child_process');
+// const path = require('path');
 
-// // This method is called when your extension is activated
-// // Your extension is activated the very first time the command is executed
+// // Python script path
+// function runPythonScript(codeSnippet) {
+//     return new Promise((resolve, reject) => {
+//         const pythonPath = path.join(__dirname, 'generate_comment.py');
 
-// /**
-//  * @param {vscode.ExtensionContext} context
-//  */
-// function activate(context) {
-
-// 	// Use the console to output diagnostic information (console.log) and errors (console.error)
-// 	// This line of code will only be executed once when your extension is activated
-// 	console.log('Congratulations, your extension "se-group6" is now active!');
-
-// 	// The command has been defined in the package.json file
-// 	// Now provide the implementation of the command with  registerCommand
-// 	// The commandId parameter must match the command field in package.json
-// 	const disposable = vscode.commands.registerCommand('se-group6.helloWorld', function () {
-// 		// The code you place here will be executed every time your command is executed
-
-// 		// Display a message box to the user
-// 		vscode.window.showInformationMessage('Hello World from SE-Group6!');
-// 	});
-
-// 	context.subscriptions.push(disposable);
+//         // Execute the Python script and pass the code snippet as a parameter
+//         execFile('python', [pythonPath, codeSnippet], (error, stdout, stderr) => {
+//             if (error) {
+//                 reject(`Error: ${stderr}`);
+//             } else {
+//                 resolve(stdout.trim());  // Return the generated comment
+//             }
+//         });
+//     });
 // }
 
-// // This method is called when your extension is deactivated
+// function activate(context) {
+//     // Command to convert selected text to uppercase
+//     let convertToUpperCase = vscode.commands.registerCommand('extension.convertToUpperCase', function () {
+//         const editor = vscode.window.activeTextEditor;
+//         if (editor) {
+//             const selection = editor.selection;
+//             const selectedText = editor.document.getText(selection);
+//             if (!selectedText) {
+//                 vscode.window.showInformationMessage('Please select text');
+//                 return;
+//             }
+//             const upperCaseText = selectedText.toUpperCase();
+//             editor.edit(editBuilder => {
+//                 editBuilder.replace(selection, upperCaseText);
+//             });
+//         }
+//     });
+
+//     // Command to convert selected text to lowercase
+//     let convertToLowerCase = vscode.commands.registerCommand('extension.convertToLowerCase', function () {
+//         const editor = vscode.window.activeTextEditor;
+//         if (editor) {
+//             const selection = editor.selection;
+//             const selectedText = editor.document.getText(selection);
+//             if (!selectedText) {
+//                 vscode.window.showInformationMessage('Please select text');
+//                 return;
+//             }
+//             const lowerCaseText = selectedText.toLowerCase();
+//             editor.edit(editBuilder => {
+//                 editBuilder.replace(selection, lowerCaseText);
+//             });
+//         }
+//     });
+
+//     // Command to generate comment for selected code
+//     let generateComment = vscode.commands.registerCommand('extension.generateComment', async function () {
+//         const editor = vscode.window.activeTextEditor;
+//         if (editor) {
+//             const selection = editor.selection;
+//             const selectedText = editor.document.getText(selection);
+//             if (!selectedText) {
+//                 vscode.window.showInformationMessage('Please select code to generate comment');
+//                 return;
+//             }
+
+//             try {
+//                 // Call the Python script to generate the comment
+//                 const commentText = await runPythonScript(selectedText);
+//                 if (commentText) {
+//                     // Insert the generated comment at the start of the selected code
+//                     editor.edit(editBuilder => {
+//                         editBuilder.insert(selection.start, `// ${commentText}\n`);
+//                     });
+//                 }
+//             } catch (error) {
+//                 vscode.window.showErrorMessage('Failed to generate comment: ' + error);
+//             }
+//         }
+//     });
+
+//     // Register the commands
+//     context.subscriptions.push(convertToUpperCase);
+//     context.subscriptions.push(convertToLowerCase);
+//     context.subscriptions.push(generateComment);
+// }
+
 // function deactivate() {}
 
 // module.exports = {
-// 	activate,
-// 	deactivate
-// }
+//     activate,
+//     deactivate
+// };
+
+
+
+
 
 
 const vscode = require('vscode');
@@ -44,6 +106,22 @@ const path = require('path');
 function runPythonScript(codeSnippet) {
     return new Promise((resolve, reject) => {
         const pythonPath = path.join(__dirname, 'generate_comment.py');
+
+        // Execute the Python script and pass the code snippet as a parameter
+        execFile('python', [pythonPath, codeSnippet], (error, stdout, stderr) => {
+            if (error) {
+                reject(`Error: ${stderr}`);
+            } else {
+                resolve(stdout.trim());  // Return the generated comment
+            }
+        });
+    });
+}
+
+// Function to call LLM API to translate text to Chinese
+function translateToChinese(codeSnippet) {
+    return new Promise((resolve, reject) => {
+        const pythonPath = path.join(__dirname, 'generate_translate.py');
 
         // Execute the Python script and pass the code snippet as a parameter
         execFile('python', [pythonPath, codeSnippet], (error, stdout, stderr) => {
@@ -117,10 +195,37 @@ function activate(context) {
         }
     });
 
+    // Command to translate selected comment to Chinese
+    let translateComment = vscode.commands.registerCommand('extension.translateComment', async function () {
+        const editor = vscode.window.activeTextEditor;
+        if (editor) {
+            const selection = editor.selection;
+            const selectedText = editor.document.getText(selection);
+            if (!selectedText) {
+                vscode.window.showInformationMessage('Please select a comment to translate');
+                return;
+            }
+
+            try {
+                // Call LLM API to translate the comment
+                const translatedText = await translateToChinese(selectedText);
+                if (translatedText) {
+                    // Replace the selected text with the translated comment
+                    editor.edit(editBuilder => {
+                        editBuilder.insert(selection.start, `// ${translatedText}\n`);
+                    });
+                }
+            } catch (error) {
+                vscode.window.showErrorMessage('Failed to translate comment: ' + error.message);
+            }
+        }
+    });
+
     // Register the commands
     context.subscriptions.push(convertToUpperCase);
     context.subscriptions.push(convertToLowerCase);
     context.subscriptions.push(generateComment);
+    context.subscriptions.push(translateComment);
 }
 
 function deactivate() {}
@@ -129,6 +234,3 @@ module.exports = {
     activate,
     deactivate
 };
-
-
-
